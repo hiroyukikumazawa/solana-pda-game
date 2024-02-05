218a3f013430cdb597a5aa304b8f7967ba405523
Masayuki <sg.pythondev@gmail.com>
2024-02-05 02:07:59 +0000

    add testcase and change user name function

--- programs/game/src/lib.rs
+++ programs/game/src/lib.rs
@@ -17,6 +17,15 @@ pub mod game {
         user_stats.bump = ctx.bumps.user_stats;
         Ok(())
     }
+    // handler function (add this next to the create_user_stats function in the game module)
+    pub fn change_user_name(ctx: Context<ChangeUserName>, new_name: String) -> Result<()> {
+        if new_name.as_bytes().len() > 200 {
+            // proper error handling omitted for brevity
+            panic!();
+        }
+        ctx.accounts.user_stats.name = new_name;
+        Ok(())
+    }
 }
 
 #[account]
@@ -39,4 +48,12 @@ pub struct CreateUserStats<'info> {
     )]
     pub user_stats: Account<'info, UserStats>,
     pub system_program: Program<'info, System>,
+}
+
+// validation struct
+#[derive(Accounts)]
+pub struct ChangeUserName<'info> {
+    pub user: Signer<'info>,
+    #[account(mut, seeds = [b"user-stats", user.key().as_ref()], bump = user_stats.bump)]
+    pub user_stats: Account<'info, UserStats>,
 }
